8ff645fcb924a2f9f38e8c712332091a6146f1a2
Andrii Krinskiy <andrewkrinskiy@gmail.com>
2020-05-30 16:59:33 +0300

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { HeaderComponent } from './main-view/header/header.component';
+import { ContentComponent } from './main-view/content/content.component';
+import { FooterComponent } from './main-view/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
